bf3303d930f70454f41f5e99dbfcca564524f48e
brijeshsingh <brijesh122004@gmail.com>
2022-08-24 22:17:20 +0530

    This is the first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+setInterval(() => {
+    setTimeout(() => {
+      let x = document.getElementById("name").innerHTML = "HI";
+      document.getElementById("name").style.color = "red";
+    }, 500);
+    setTimeout(() => {
+      let x = document.getElementById("name").innerHTML = "HI I";
+      document.getElementById("name").style.color = "yellow";
+    }, 1000);
+    setTimeout(() => {
+      document.getElementById("name").innerHTML = "HI I AM";
+      document.getElementById("name").style.color = "blue";
+    }, 1500);
+    setTimeout(() => {
+      document.getElementById("name").innerHTML = "HI I AM BRIJESH";
+      document.getElementById("name").style.color = "gray";
+    }, 2000);
+    setTimeout(() => {
+      document.getElementById("name").innerHTML = "HI I AM BRIJESH SINGH";
+      document.getElementById("name").style.color = "green";
+    }, 2500);
+    setTimeout(() => {
+        document.getElementById("name").innerHTML = "HI I AM BRIJESH";
+        document.getElementById("name").style.color = "gray";
+      }, 3000);
+      setTimeout(() => {
+        document.getElementById("name").innerHTML = "HI I AM";
+        document.getElementById("name").style.color = "blue";
+      }, 3500);
+      setTimeout(() => {
+        document.getElementById("name").innerHTML = "HI I ";
+        document.getElementById("name").style.color = "blue";
+      }, 4000);
+      setTimeout(() => {
+        document.getElementById("name").innerHTML = "HI";
+        document.getElementById("name").style.color = "blue";
+      }, 4500);
+  }, 5000);
