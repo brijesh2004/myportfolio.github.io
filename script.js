3a6cc2a622a26e799868cf5425d831d2587ee412
brijesh2004 <brskumar0102@gmail.com>
2023-01-31 14:21:27 +0530

    enabling dark mode

--- script.js
+++ script.js
@@ -45,3 +45,50 @@ function Changehero(){
 function ChangeAgain(){
   hero.src="Images/hero.jpg";
 }
+
+const body = document.getElementById("body");
+const about = document.getElementById("about");
+const education = document.getElementById("education");
+const skill = document.getElementById("skills");
+const skil = document.getElementById("skill");
+const project = document.getElementById("project");
+const contect = document.getElementById("contect");
+
+
+
+function fun(){
+  if(body.style.backgroundColor=="rgb(185, 171, 171)"){
+    body.style.backgroundColor="rgb(1, 1, 41)";
+  }
+  else{
+    body.style.backgroundColor="rgb(185, 171, 171)";
+  }
+  if(about.style.backgroundColor=="white"){
+    about.style.backgroundColor="black";
+    about.style.color="white";
+    education.style.backgroundColor="black";
+    education.style.color="white";
+    skill.style.backgroundColor="black";
+    skill.style.color="white";
+    skil.style.color="white";
+    project.style.backgroundColor="black";
+    project.style.color="white";
+    contect.style.backgroundColor="black";
+    contect.style.color="white";
+  }
+  else{
+    about.style.backgroundColor="white";
+    about.style.color="black";
+    education.style.backgroundColor="white";
+    education.style.color="black";
+    skill.style.backgroundColor="white";
+    skill.style.color="black";
+    skil.style.color="black";
+    project.style.backgroundColor="white";
+    project.style.color="black";
+    contect.style.backgroundColor="white";
+    contect.style.color="black";
+  }
+
+  
+}
